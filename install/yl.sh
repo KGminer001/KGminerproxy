71ee988e51445519f5d8dca390f2da145a4c448f
Youareman001 <164907244+Youareman001@users.noreply.github.com>
2024-09-20 22:45:36 +0800

    Update yl.sh

--- install/yl.sh
+++ install/yl.sh
@@ -1,1 +1,1 @@
-bash <(curl -s -L https://raw.githubusercontent.com/Youareman001/frp/main/install_tb.sh) YLMinrproxy https://raw.githubusercontent.com/Youareman001/frp/main/frfile yl_franchisee.bin
+bash <(curl -s -L https://raw.githubusercontent.com/Youareman001/frp/main/base_install.sh) YLMinrproxy https://raw.githubusercontent.com/Youareman001/frp/main/frfile yl_franchisee.bin
