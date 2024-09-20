644547c059f212c7a125c8a80506f154066fd02a
Youareman001 <164907244+Youareman001@users.noreply.github.com>
2024-09-20 23:34:53 +0800

    Update base_install.sh

--- base_install.sh
+++ base_install.sh
@@ -2,9 +2,9 @@
 
 # 需要修改的配置不明白最好只修改下载地址和核心配置文件下载地址其他配置请勿修改
 version='v13.1.8'
-shell_version='5.2' #脚本版本
+shell_version='5.3' #脚本版本
 uiname=$1'-shell' #脚本名称
-appinstalname='fxcusminer'-$version #软件安装包名称
+appinstalname='fxcusminerlinux'-$version #软件安装包名称
 sofname=$1miner #软件名称
 wdog='running'$1 #看门狗名称不能和软件名称相同最好一个字母都不相同
 installdirName='fx-'$1'-miner' #安装文件夹名
