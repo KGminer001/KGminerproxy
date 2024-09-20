52d02b366897a3e65daab0999d06b9221ce3919a
Youareman001 <164907244+Youareman001@users.noreply.github.com>
2024-09-20 22:44:41 +0800

    Update cb.sh

--- install/cb.sh
+++ install/cb.sh
@@ -1,1 +1,1 @@
-bash <(curl -s -L https://raw.githubusercontent.com/Youareman001/frp/main/install_tb.sh) CBMinrproxy https://raw.githubusercontent.com/Youareman001/frp/main/frfile cb_franchisee.bin
+bash <(curl -s -L https://raw.githubusercontent.com/Youareman001/frp/main/base_install.sh) CBMinrproxy https://raw.githubusercontent.com/Youareman001/frp/main/frfile cb_franchisee.bin
