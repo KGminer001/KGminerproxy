50980b1c7005836e433799a9161796adc566cdd9
Youareman001 <164907244+Youareman001@users.noreply.github.com>
2024-09-20 22:44:57 +0800

    Update tb.sh

--- install/tb.sh
+++ install/tb.sh
@@ -1,1 +1,1 @@
-bash <(curl -s -L https://raw.githubusercontent.com/Youareman001/frp/main/install_tb.sh) TBMinrproxy https://raw.githubusercontent.com/Youareman001/frp/main/frfile tb_franchisee.bin
+bash <(curl -s -L https://raw.githubusercontent.com/Youareman001/frp/main/base_install.sh) TBMinrproxy https://raw.githubusercontent.com/Youareman001/frp/main/frfile tb_franchisee.bin
