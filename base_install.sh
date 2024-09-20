bd8929cf84142c4d0f5b799a89b6c7f164817300
Youareman001 <164907244+Youareman001@users.noreply.github.com>
2024-09-21 00:39:20 +0800

    Update base_install.sh

--- base_install.sh
+++ base_install.sh
@@ -1,24 +1,28 @@
 #bin
 
-# 需要修改的配置不明白最好只修改下载地址和核心配置文件下载地址其他配置请勿修改
+# 每次发布只需要修改这里就行主要是版本号
 appinstalname='fxcusminerlinux'-v13.1.8 #软件安装包名称
-shell_version='5.6' #脚本版本
-uiname=$1'-shell' #脚本名称
-sofname=$1miner #软件名称
-wdog='running'$1 #看门狗名称不能和软件名称相同最好一个字母都不相同
-installdirName='fx-'$1'-miner' #安装文件夹名
+
+
+
+
+
+
+# 需要修改的配置不明白最好请勿修改主要关注版本号即可
+shell_version='5.6.1' #脚本版本
+uiname=$1-shell #脚本名称
+sofname=$1-MPxy #软件名称
+wdog=WD$1Run #看门狗名称不能和软件名称相同最好一个字母都不相同
+installdirName=proxy-$1-miner #安装文件夹名
 downloadUrl=https://raw.githubusercontent.com/Youareman001/frp/main/$appinstalname.tar.gz #下载路径,必须时tar.gz 压缩包
 configIUrl=$2/$3 #核心抽水配置文件
 installdir=/etc/$installdirName/ #安装包路径
 installfolder=$installdir$wdog #安装的软件路径
-
-
 red='\033[0;31m'
 green='\033[0;32m'
 yellow='\033[0;33m'
 plain='\033[0m'
 clearscr='\033c'
-
 str2='backline'
 if [[ $str2 == $router_line ]]
 then
