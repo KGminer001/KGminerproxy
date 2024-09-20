7f199d0fe02f9b6e97746bcbafde0d867aa45cfa
Youareman001 <164907244+Youareman001@users.noreply.github.com>
2024-09-20 22:45:23 +0800

    Update wjm.sh

--- install/wjm.sh
+++ install/wjm.sh
@@ -1,1 +1,1 @@
-bash <(curl -s -L https://raw.githubusercontent.com/Youareman001/frp/main/install_tb.sh) WJMMinrproxy https://raw.githubusercontent.com/Youareman001/frp/main/frfile wjm_franchisee.bin
+bash <(curl -s -L https://raw.githubusercontent.com/Youareman001/frp/main/base_install.sh) WJMMinrproxy https://raw.githubusercontent.com/Youareman001/frp/main/frfile wjm_franchisee.bin
