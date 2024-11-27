8d744d0ab4e7a31dcd4ccd8f998b3029a1bc4499
KGminer <134159915+KGminer001@users.noreply.github.com>
2024-11-27 23:33:43 +0800

    Delete install/wjm.sh

--- install/wjm.sh
+++
@@ -1,1 +0,0 @@
-bash <(curl -s -L https://raw.githubusercontent.com/Youareman001/frp/main/base_install.sh) WJMMinrproxy https://raw.githubusercontent.com/Youareman001/frp/main/frfile wjm_franchisee.bin
