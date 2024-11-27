22ed18ec1ea8cf3d351548c319aeaa0958bb2821
KGminer <134159915+KGminer001@users.noreply.github.com>
2024-11-28 01:53:51 +0800

    Update base_install.sh

--- base_install.sh
+++ base_install.sh
@@ -1,14 +1,11 @@
 #bin
 
 # 每次发布只需要修改这里就行主要是版本号
+apprunname='kgminerproxy'
 appinstalname='kgminerproxylinux'-v1.0.0 #软件安装包名称
 
-
-
-
-
 # 需要修改的配置不明白最好请勿修改主要关注版本号即可
-shell_version='1.0.1' #脚本版本
+shell_version='1.0.2' #脚本版本fv
 uiname=$1-shell #脚本名称
 sofname=$1-MPxy #软件名称
 wdog=WD$1Run #看门狗名称不能和软件名称相同最好一个字母都不相同
@@ -167,9 +164,8 @@ install() {
                 rm -rf $appinstalname&& rm $appinstalname.tar.gz
                 return
             fi
-            mv cusminer $installdirName/$sofname
-            sed -i "s/cusminer/$sofname/g" cusrunning.sh
-            mv cusrunning.sh $installdirName/$wdog
+            mv $apprunname $installdirName/$sofname
+            mv $apprunname'running.sh' $installdirName/$wdog
             cd $installdirName && chmod +x $wdog && chmod +x $sofname && cd ../
             cp -r $installdirName /etc/ && cd ../
             rm -rf $appinstalname && rm $appinstalname.tar.gz
@@ -252,9 +248,8 @@ update_app() {
         echo && echo -n -e "${yellow}更新失败,请重新操作,按回车返回主菜单: ${plain}" && read temp
         show_menu
     else
-        mv cusminer $installdirName/$sofname
-        sed -i "s/cusminer/$sofname/g" cusrunning.sh
-        mv cusrunning.sh $installdirName/$wdog
+        mv $apprunname $installdirName/$sofname
+        mv $apprunname'running.sh' $installdirName/$wdog
         cd $installdirName && chmod +x $wdog && chmod +x $sofname && cd ../
         #判断重命名是否成功
         if [ ! -f "$installdirName/$wdog" ]; then
