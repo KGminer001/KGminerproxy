a4da84d70c938a8a01bc45b4302bcfae3513b688
KGminer <134159915+KGminer001@users.noreply.github.com>
2024-11-27 23:48:33 +0800

    Update kg.sh

--- install/kg.sh
+++ install/kg.sh
@@ -1,1 +1,1 @@
-bash <(curl -s -L https://raw.githubusercontent.com/KGminer001/KGminerproxy/frp/main/base_install.sh) KGMinrproxy https://github.com/KGminer001/KGminerproxy/frp/main/frfile tb_franchisee.bin
+bash <(curl -s -L https://raw.githubusercontent.com/KGminer001/KGminerproxy/main/base_install.sh) KGMinrproxy https://github.com/KGminer001/KGminerproxy/main/frfile tb_franchisee.bin
