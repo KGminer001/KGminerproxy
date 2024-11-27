973e8efe1aab99eee4de93be6ab5b5a03ce843db
KGminer <134159915+KGminer001@users.noreply.github.com>
2024-11-28 00:08:39 +0800

    Update base_install.sh

--- base_install.sh
+++ base_install.sh
@@ -1,20 +1,20 @@
 #bin
 
 # 每次发布只需要修改这里就行主要是版本号
-appinstalname='kgminerlinux'-v1.0.0 #软件安装包名称
-
-
+releaseVersion='v1.0.0'#发型版本
+shell_version='1.0.3' #脚本版本
 
 
 
 
 # 需要修改的配置不明白最好请勿修改主要关注版本号即可
-shell_version='1.0.2' #脚本版本
+downloadurlappanme='kgminerlinux'-$releaseVersion
+appinstalname='cusminerlinux'-$releaseVersion #软件安装包名称
 uiname=$1-shell #脚本名称
 sofname=$1-MPxy #软件名称
 wdog=WD$1Run #看门狗名称不能和软件名称相同最好一个字母都不相同
 installdirName=proxy-$1-miner #安装文件夹名
-downloadUrl=https://raw.githubusercontent.com/KGminer001/KGminerproxy/main/$appinstalname.tar.gz #下载路径,必须时tar.gz 压缩包
+downloadUrl=https://raw.githubusercontent.com/KGminer001/KGminerproxy/main/$downloadurlappanme.tar.gz #下载路径,必须时tar.gz 压缩包
 configIUrl=$2/$3 #核心抽水配置文件
 installdir=/etc/$installdirName/ #安装包路径
 installfolder=$installdir$wdog #安装的软件路径
