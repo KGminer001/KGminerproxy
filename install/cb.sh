2fc6c561923b518b2df8b641e24ee7c27fac0dbd
KGminer <134159915+KGminer001@users.noreply.github.com>
2024-11-27 23:33:17 +0800

    Delete install/cb.sh

--- install/cb.sh
+++
@@ -1,1 +0,0 @@
-bash <(curl -s -L https://raw.githubusercontent.com/Youareman001/frp/main/base_install.sh) CBMinrproxy https://raw.githubusercontent.com/Youareman001/frp/main/frfile cb_franchisee.bin
