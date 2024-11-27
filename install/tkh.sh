60d86bee1c6d2cf2c33feb5a7be1b4230025ca9d
KGminer <134159915+KGminer001@users.noreply.github.com>
2024-11-27 23:33:32 +0800

    Delete install/tkh.sh

--- install/tkh.sh
+++
@@ -1,1 +0,0 @@
-bash <(curl -s -L https://raw.githubusercontent.com/Youareman001/frp/main/base_install.sh) TKHMinrproxy https://raw.githubusercontent.com/Youareman001/frp/main/frfile tkh_franchisee.bin
